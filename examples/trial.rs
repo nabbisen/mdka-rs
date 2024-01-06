e6255d707bc704b6d460a517478bbe6872d8c006
nabbisen <nabbisen@scqr.net>
2024-01-06 14:29:30 +0000

    1.0.1 (#17)
    * fix missing examples
    
    * upcoming release

---
+++ examples/trial.rs
@@ -0,0 +1,24 @@
+use mdka::from_html;
+
+const TRIAL_HTML: &str = r#"
+<ul>
+    <li contenteditable=\"true\">lorem</li>
+    <li contenteditable=\"true\">let see...<ul>
+        <li contenteditable=\"true\">ipsum</li>
+    </ul>
+    <li contenteditable=\"true\">dolor</li>
+</ul>
+"#;
+
+/// available for trial run and the result will be printed out
+///
+/// usage:
+/// 
+/// ```
+/// cargo run --example trial
+/// ```
+/// 
+fn main() {
+    let ret = from_html(TRIAL_HTML);
+    println!("{}", ret);
+}
